791c7d9141f47bebad8fc0218e4ae614d8049d1d
nicodibi <nicolas.dibiase22@gmail.com>
2017-06-08 15:59:09 -0300

    Excepciones

--- src/datos/JsonLector.java
+++ src/datos/JsonLector.java
@@ -12,27 +12,21 @@ import com.google.gson.stream.JsonReader;
 
 public class JsonLector<T> {
 
-	public List<T> getArrayObject(String nombreArchivo) {
+	public List<T> getArrayObject(String nombreArchivo) throws FileNotFoundException {
 		
 		Type listType = new TypeToken<ArrayList<T>>(){}.getType();
 		List<T> yourClassList = new ArrayList<T>();
-		try {
-			JsonReader reader = new JsonReader(new FileReader(nombreArchivo));
-			yourClassList = new Gson().fromJson(reader, listType);
-		}catch(Exception e) {
-			
-		}
+		JsonReader reader = new JsonReader(new FileReader(nombreArchivo));
+		yourClassList = new Gson().fromJson(reader, listType);
 		return yourClassList;
 	}
 	
 	public T getObject(String nombreArchivo) throws FileNotFoundException {
 		
 		Type type = new TypeToken<T>(){}.getType();
-		T yourClassList;
-		
+		T yourClassList;		
 		JsonReader reader = new JsonReader(new FileReader(nombreArchivo));
-		yourClassList = new Gson().fromJson(reader, type);
-		
+		yourClassList = new Gson().fromJson(reader, type);		
 		return yourClassList;
 	}
 }
